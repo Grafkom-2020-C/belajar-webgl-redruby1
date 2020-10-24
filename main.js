77da8d533858221b1f9fcea03da3a8d1741e3c21
redruby1 <anisa.aura229@gmail.com>
2020-10-24 14:24:17 +0700

    Berhasil membuat persegi gradasi dengan gl.TRIANGLES

--- main.js
+++ main.js
@@ -7,13 +7,16 @@ function main() {
         A = (-0.5, 0.5)
         B = (0.5, 0.5)
         C = (0.5, -0.5)
+        D = (-0.5, -0.5)
     */
 
     var vertices = [
-        0.5, 0.5,       // Titik B (kanan-atas)
-        -0.5, 0.5,      // Titik A (kiri-atas)
-        0.5, -0.5,      // Titik C (kanan-bawah)
-        -0.5, -0.5      // Titik D (kiri-bawah)
+        -0.5, 0.5, 1.0, 0.0, 0.0,      // Titik A (kiri-atas)
+        0.5, 0.5, 0.0, 1.0, 0.0,       // Titik B (kanan-atas)
+        0.5, -0.5, 0.0, 0.0, 1.0,      // Titik C (kanan-bawah)
+        -0.5, 0.5, 1.0, 0.0, 0.0,      // Titik A 
+        0.5, -0.5, 0.0, 0.0, 1.0,      // Titik C 
+        -0.5, -0.5, 0.0, 1.0, 0.0      // Titik D (kiri-bawah)
     ];
 
     var vertexBuffer = gl.createBuffer();
@@ -25,13 +28,18 @@ function main() {
     // dibawah ini .c
     var vertexShaderSource = `
         attribute vec2 a_Position;
+        attribute vec3 a_Color;
+        varying vec3 v_Color;
         void main() {
             gl_Position = vec4(a_Position, 0.0, 1.0); // posisi/koordinat titik
+            v_Color = a_Color;
         }
     `;
     var fragmentShaderSource = `
+        precision mediump float;
+        varying vec3 v_Color;
         void main() {
-            gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0); // warna titik
+            gl_FragColor = vec4(v_Color, 1.0); // warna gambar
         }
     `;
     
@@ -62,14 +70,34 @@ function main() {
 
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     var aPosition = gl.getAttribLocation(shaderProgram, "a_Position");
-    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    var aColor = gl.getAttribLocation(shaderProgram, "a_Color");
+    
+    gl.vertexAttribPointer(
+        aPosition,
+        2,
+        gl.FLOAT,
+        false,
+        5 * Float32Array.BYTES_PER_ELEMENT,
+        0
+    );
+
+    gl.vertexAttribPointer(
+        aColor,
+        3,
+        gl.FLOAT,
+        false,
+        5 * Float32Array.BYTES_PER_ELEMENT,
+        2 * Float32Array.BYTES_PER_ELEMENT
+    );
+    
     gl.enableVertexAttribArray(aPosition);
+    gl.enableVertexAttribArray(aColor);
 
     gl.clearColor(0.0, 255.0, 255.0, 0.8); // warna kotak
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    var primitive = gl.TRIANGLE_STRIP;
+    var primitive = gl.TRIANGLES;
     var offset = 0;
-    var nVertex = 4;
+    var nVertex = 6;
     gl.drawArrays(primitive, offset, nVertex);
 }
